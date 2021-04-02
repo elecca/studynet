5710e9a8d0ecfe29bb850e0bef3b6b4bac1071b0
Eddy Lecca <eddy.lecca@gmail.com>
2021-04-02 14:32:10 -0500

    subida de proyecto

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+use App\Models\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'Eddy Oscar Lecca Ricra',
+            'email' => 'eddy.lecca@gmail.com',
+            'password' => bcrypt('12345678')
+        ]);
+
+
+        $user->assignRole('Admin');
+
+        User::factory(99)->create();
+    }
+}
